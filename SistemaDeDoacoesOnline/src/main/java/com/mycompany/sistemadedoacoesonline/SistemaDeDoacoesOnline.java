950e78b456ccbd84a08d3f9f10f6c33c70f6a4dd
ViniciusSouzaSilva <vinisic331@gmail.com>
2023-12-03 15:24:32 -0300

    Commit inicial e conector JDBC

---
+++ SistemaDeDoacoesOnline/src/main/java/com/mycompany/sistemadedoacoesonline/SistemaDeDoacoesOnline.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadedoacoesonline;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class SistemaDeDoacoesOnline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
